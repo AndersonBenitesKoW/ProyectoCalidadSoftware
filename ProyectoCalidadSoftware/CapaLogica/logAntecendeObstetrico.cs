f32aa57ddf0051a700b8a8b87f83aac4e2c62c98
Completoo <andreponester@gmail.com>
2025-10-03 23:12:28 -0500

    gringo

---
+++ ProyectoCalidadSoftware/CapaLogica/logAntecendeObstetrico.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class logAntecendeObstetrico
+    {
+
+        #region Singleton
+        private static readonly logAntecedenteObstetrico _instancia = new logAntecedenteObstetrico();
+        public static logAntecedenteObstetrico Instancia
+        {
+            get { return logAntecedenteObstetrico._instancia; }
+        }
+        #endregion
+
+        #region Métodos
+
+        public DataTable ListarAntecedenteObstetrico()
+        {
+            return DA_AntecedenteObstetrico.Instancia.Listar();
+        }
+
+        public bool InsertarAntecedenteObstetrico(int idPaciente, short? menarquia, short? cicloDias, short? gestas,
+                                                  short? partos, short? abortos, string observacion, bool estado)
+        {
+            return DA_AntecedenteObstetrico.Instancia.Insertar(
+                idPaciente, menarquia, cicloDias, gestas, partos, abortos, observacion, estado
+            );
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ ProyectoCalidadSoftware/CapaLogica/logAuditoria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+   public class logAuditoria
+    {
+
+        #region Singleton
+        private static readonly logAuditoria _instancia = new logAuditoria();
+        public static logAuditoria Instancia
+        {
+            get { return logAuditoria._instancia; }
+        }
+        #endregion
+
+        #region Métodos
+
+        public DataTable ListarAuditoria()
+        {
+            return DA_Auditoria.Instancia.Listar();
+        }
+
+        public bool InsertarAuditoria(int? idUsuario, string accion, string entidad, int? idRegistro,
+                                      string antes, string despues, string ipCliente)
+        {
+            return DA_Auditoria.Instancia.Insertar(idUsuario, accion, entidad, idRegistro, antes, despues, ipCliente);
+        }
+
+        #endregion
+
+
+    }
+}
